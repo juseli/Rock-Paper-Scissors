1f995f1c17c1eb2fbaf073397a661de2ac997670
Justin Li <li.justinjl@gmail.com>
2023-01-10 00:05:34 -0500

    .

--- script_rps.js
+++ script_rps.js
@@ -1,77 +1,109 @@
 
-  let playerScore = 0;
-  let computerScore = 0;
 
-    function computerChoice () {
-    let randomNumber = Math.floor(Math.random() *3) +1;
+document.getElementById("rock").onclick = game;
+document.getElementById("paper").onclick = game;
+document.getElementById("scissors").onclick = game;
+document.getElementById("reset").onclick = reset;
 
-        if (randomNumber === 1) {
-        return "rock";
-        } else if (randomNumber === 2) {
-        return "paper";
+
+document.querySelector("#score");
+score.textContent = "Player Score: 0 Computer Score: 0 ";
+
+function reset () {
+  document.getElementById("rock").disabled = false;
+  document.getElementById("paper").disabled = false;
+  document.getElementById("scissors").disabled = false;
+  score.textContent = "Player Score: 0 Computer Score: 0 ";
+  playerScore = 0
+  computerScore = 0
+  
+}
+
+  let playerScore = 0
+  let computerScore = 0
+
+function game(){
+    
+    
+
+    let playerSelection = this.id;
+    
+    
+    let computerSelection = Math.floor(Math.random() *3) +1;
+
+        if (computerSelection === 1) {
+        computerSelection = "rock"  ;
+        } else if (computerSelection === 2) {
+        computerSelection = "paper";
         } else {
-        return "scissors";
+        computerSelection = "scissors";
         }
-    }
+  
+    console.log("Computer Choice:" + computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+
     
-  let playerRoundWin = "Player wins this round!"
-  let computerRoundWin = "Computer wins this round!"
-  let draw = "Draw!"  
-    
-  function playRound(playerSelection , computerSelection) {
+    function playRound(playerSelection , computerSelection) {
 
-    if (playerSelection === "scissors" && computerSelection === "paper") {
-      return playerRoundWin;
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-      return computerRoundWin;
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-      return playerRoundWin;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-      return playerRoundWin;
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-      return computerRoundWin;
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-      return computerRoundWin;
-    } else {
-      return draw;
-    } 
-  
-    }
-
-    for (let i = 0; i < 99; i++) {
-      let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-      let computerSelection = computerChoice();
-      let roundResult = playRound(playerSelection , computerSelection);
-      console.log(roundResult);
-      score(roundResult);
-      console.log("Your score is" + " " + playerScore);
-      console.log("The computer score is" + " " + computerScore);
-  
-      if (playerScore === 3 ) {
-        alert("You win!")
-      }
-
-      if (computerScore === 3) {
-        alert("You lose!")
-      }
-
-      if (playerScore === 3 || computerScore === 3) {
-        break;
-      }
-
-    function score(result) {
-  
-      if (result === playerRoundWin) {
-        playerScore++;
-      }else if (result === computerRoundWin) {
-        computerScore++;
-      }else {
-        playerScore && computerScore + 0;
-      }
-
-
+      let round = ""
+      let score = ""
       
-      }
 
-    
+      if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
+        round = ("Player wins this round! " + playerSelection + " beats " + computerSelection + "!");
+        score = ("Player Score: " + playerScore + " Computer Score: "  + computerScore);
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++;
+        round = ("Computer wins this round! " + computerSelection + " beats " + playerSelection + "!");
+        score = ("Player Score: " + playerScore + " Computer Score: "  + computerScore);
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++;
+        round = ("Player wins this round! " + playerSelection + " beats " + computerSelection + "!");
+        score = ("Player Score: " + playerScore + " Computer Score: "  + computerScore);
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
+        round = ("Player wins this round! " + playerSelection + " beats " + computerSelection + "!");
+        score = ("Player Score: " + playerScore + " Computer Score: "  + computerScore);
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
+        round = ("Computer wins this round! " + computerSelection + " beats " + playerSelection + "!");
+        score = ("Player Score: " + playerScore + " Computer Score: "  + computerScore);
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++;
+        round = ("Computer wins this round! " + computerSelection + " beats " + playerSelection + "!");
+        score = ("Player Score: " + playerScore + " Computer Score: "  + computerScore);
+    } else {
+        playerScore && computerScore +0; 
+        round = ("Tie!");
+        score = ("Player Score: " + playerScore + " Computer Score: "  + computerScore);
+    } 
+
+    document.getElementById("round").innerHTML = round
+    document.getElementById("score").innerHTML = score
+    return
+
     }
+
+    let result = ""
+
+    if (playerScore === 5) {
+      result = ("Player wins!")
+      document.getElementById("rock").disabled = true;
+      document.getElementById("paper").disabled = true;
+      document.getElementById("scissors").disabled = true;
+    }else if (computerScore === 5) {
+      result = ("Computer wins!")
+      document.getElementById("rock").disabled = true;
+      document.getElementById("paper").disabled = true;
+      document.getElementById("scissors").disabled = true;
+    }
+
+    document.getElementById("result").innerHTML = result
+    
+}
+
+  
+
+  
+ 
